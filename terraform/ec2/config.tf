919a8e1cb608b64ea067b748dbea9a7a1168db63
Horst Osimitz <horstosimitz@MacbookProM4ProOH.local>
2025-06-11 12:35:17 +0200

    Move terraform backend to S3

--- terraform/ec2/config.tf
+++ terraform/ec2/config.tf
@@ -1,12 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "<= 5.72.1" # https://github.com/hashicorp/terraform-provider-aws/issues/40091
     }
   }
 
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket = "tf-backend-rgitoh"
+    key    = "demo/ec2/terraform.tfstate"
+    region = "eu-central-1"
   }
 }
 
